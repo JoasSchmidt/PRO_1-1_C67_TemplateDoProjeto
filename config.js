13e8d93f2d8df0fc9f07c2441b9f985a4708d6cb
JoasSchmidt <101359176+JoasSchmidt@users.noreply.github.com>
2023-06-05 16:54:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
+var firebaseConfig = {const firebaseConfig = {
+  apiKey: "AIzaSyASFK_I2AWAqED-flF0NxKerMpHcHNRd6g",
+  authDomain: "votecomnosco.firebaseapp.com",
+  databaseURL: "https://votecomnosco-default-rtdb.firebaseio.com",
+  projectId: "votecomnosco",
+  storageBucket: "votecomnosco.appspot.com",
+  messagingSenderId: "438093618520",
+  appId: "1:438093618520:web:dda2e2cf4db8ea8a98d92d"
+};
 
 };
 // Initialice o Firebase
